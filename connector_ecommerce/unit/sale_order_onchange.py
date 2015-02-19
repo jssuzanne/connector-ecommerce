bf82f1c2a534f421da8085bb0edc2cd206baa41c
Guewen Baconnier <guewen@gmail.com>
2015-02-19 09:42:18 +0100

    No shop_id, no more 'sale.shop'

--- connector_ecommerce/unit/sale_order_onchange.py
+++ connector_ecommerce/unit/sale_order_onchange.py
@@ -224,8 +224,7 @@ class SaleOrderOnChange(OnChangeManager):
         :rtype: dict
         """
         # play onchange on sale order
-        with self.session.change_context(dict(shop_id=order.get('shop_id'))):
-            order = self._play_order_onchange(order)
+        order = self._play_order_onchange(order)
         # play onchange on sale order line
         processed_order_lines = []
         line_lists = [order_lines]
